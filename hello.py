f7a8659afe84b10140dfbdfc3436eb784f6bfb7d
Astryda Malinowska <astamalinowska@outlook.com>
2022-10-18 20:38:23 +0200

    Added dynamic route

--- hello.py
+++ hello.py
@@ -2,8 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Routing
 @app.route('/')
 def index():
     return '<h1>Hello Flask world!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, dear {}!</h1>'.format(name)
 
 #flask --app hello run
